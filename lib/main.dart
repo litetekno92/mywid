5739f2b7fce7b27f0dd783385bd2cf263205ae64
LiteTekno92 <lite.tekno92@gmail.com>
2019-04-02 08:07:19 +0200

    All added

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
+import 'widgets/mywidget.dart';
+
+void main() => runApp(MyWidget());
